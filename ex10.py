d6c63dbb53a72d87dddbb1b90c5ea3d540bd2707
richard wahl <richard wahl>
2016-12-26 14:35:13 -0800

    Added existing files

---
+++ ex10.py
@@ -0,0 +1,8 @@
+print ("How old are you?"),
+age = int(input())
+print ("How tall are you?"),
+height = input()
+print ("How much do you weight?"),
+weight = input()
+print ("so you are {} old, {} tall, and {} heavy".format(age, height, weight))
+exit()
---
+++ ex13.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, user_name = argv
+prompt = ">"
+print ("Hi {}, I'm the {}".format(argv[1],script))
